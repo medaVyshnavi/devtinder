61c1f0d3f9bca4d43924bf7ad2a1092569b824a7
medaVyshnavi <vyshnavivenkatesh@gmail.com>
2025-05-20 21:20:39 +0900

    route handlers and middlewares

---
+++ README.md
@@ -0,0 +1,2 @@
+tOSVtWIW08RJgWYa
+mongodb+srv://vyshnavivenkatesh:tOSVtWIW08RJgWYa@cluster0.j2l7cge.mongodb.net/
--- src/app.js
+++ src/app.js
@@ -3,19 +3,44 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/", (req, res,next) => {
-  console.log("my express server says hello");
-  next();
+app.use("/test", (req, res) => {
+  throw new Error("erooorrrrr")
+})
+
+app.use("/", (req, res) => {
+  throw new Error("generic error");
 });
 
-app.get("/users", (req, res) => {
-  res.send({name:"vyshnavi",place:"tokyo"})
-});
+app.use((error, req, res, next) => {
+  res.status(500).send(error.message)
+})
 
-app.post("/users", (req, res) => {
-  console.log(req.body);
-  res.send("data recieevd ")
-});
+// app.use((req, res, next) => {
+//   res.send("bye bye")
+// })
+
+// app.use(
+//   "/",
+//   (req, res, next) => {
+//     console.log("my express server says hello once");
+//      next();
+//     res.send("okay coming")
+//   },
+//   (req, res, next) => {
+//     console.log("my express server says hello twice");
+//     next();
+//     res.send("okay wait");
+//   }
+// );
+
+// app.get("/users", (req, res) => {
+//   res.send({name:"vyshnavi",place:"tokyo"})
+// });
+
+// app.post("/users", (req, res) => {
+//   console.log(req.body);
+//   res.send("data recieevd ")
+// });
 
 app.listen(3001, () => {
   console.log("listening to port 3001")
