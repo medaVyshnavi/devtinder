632b27fca4100ab75e417083ea99acb5698ef54d
medaVyshnavi <vyshnavivenkatesh@gmail.com>
2025-05-22 21:31:15 +0900

    apis

--- src/app.js
+++ src/app.js
@@ -7,12 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.post("/signUp", async(req, res) => {
-  const userObj = {
-    firstName: "vysh",
-    lastName: "venkatesh",
-    email: "vysh@gmail.com",
-    password: "123456",
-  };
+  const userObj = req.body;
 
   try {
     const user = new User(userObj);
@@ -20,6 +15,25 @@ app.post("/signUp", async(req, res) => {
     res.send("saved user to db");
   } catch (error) {
     res.status(400).send(error)
+  }
+})
+
+app.get("/feed", async (req, res) => {
+  try {
+    const usersList = await User.find();
+    res.send(usersList);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+})
+
+app.get("/user", async (req, res) => {
+  const email = req.query.emailId
+  try {
+    const user = await User.find({emailId : email});
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
   }
 })
 
