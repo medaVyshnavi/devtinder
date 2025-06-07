a44d0d37f27d8d0198c91d344ca51983f2561f6a
medaVyshnavi <vyshnavivenkatesh@gmail.com>
2025-06-07 21:28:17 +0900

    feed API

--- apiList.md
+++ apiList.md
@@ -17,7 +17,7 @@
 ## userRouter
 - GET /user/requests/received => interested
 - GET /user/connections
-- GET /user/feed - Gets you the profiles of other users on platform
+- GET /user/feed - Gets you the profiles of other users on platform (where you dont show already connected, rejected  accepted or ignored and also his own card)
 
 
 Status: ignored, interested, accepeted, rejected
--- src/router/user.js
+++ src/router/user.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const userRouter = express.Router();
 const { userAuthentication } = require("../middlewares/auth")
-const {ConnectionRequest} = require("../models/connectionRequestSchema")
+const {ConnectionRequest} = require("../models/connectionRequestSchema");
+const { User } = require("../models/userSchema");
 
 userRouter.get("/requests/received", userAuthentication, async (req, res) => {
   try {
@@ -48,5 +49,47 @@ userRouter.get("/connections", userAuthentication, async (req, res) => {
     res.status(400).send({message:error})
   }
 });
+
+// in the feed api i have to show the users which are not accepted rejected ignored 
+// or intrested. so i get all those id of the users that have already one of those status 
+// with loggedin user as from or to user id. then filter them all from the user list based 
+// on the email id that is unique.
+
+userRouter.get("/feed", userAuthentication, async(req, res) => {
+  try {
+    const loggedInUser = req.user
+    const accessedProfiles = await ConnectionRequest.find({
+      $or: [
+        {
+          fromUserId: loggedInUser._id,
+          status: { $in: ["interested", "rejected", "accepted", "ignore"] },
+        },
+        {
+          toUserId: loggedInUser._id,
+          status: { $in: ["interested", "rejected", "accepted", "ignore"] },
+        },
+      ],
+    }).populate("fromUserId", ["email"])
+      .populate("toUserId", ["email"]);
+  
+    
+    const objectIds = accessedProfiles.map(profile => {
+      if (profile.fromUserId._id.equals(loggedInUser._id)) {
+        return profile.toUserId._id
+      }
+      return profile.fromUserId._id
+    })
+
+    objectIds.push(loggedInUser._id)
+
+    const feedList = await User.find({ _id: { $nin: objectIds } }).select(
+      "firstName lastName dob gender hobbies photoURL about"
+    );
+    res.status(200).json({message : "Sucessfully Fetched the feed data", data:feedList})
+    
+  } catch (error) {
+    res.status(400).send({message:error})
+  }
+})
 
 module.exports = userRouter
