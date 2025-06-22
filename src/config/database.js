e4ad4c58bda1a929d9044edeef455e553984b57d
medaVyshnavi <vyshnavivenkatesh@gmail.com>
2025-06-22 15:50:28 +0900

    db setup

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose")
 
-const connectionDB = async() => {
-  await mongoose.connect(
-    "mongodb+srv://vyshnavivenkatesh:tOSVtWIW08RJgWYa@cluster0.j2l7cge.mongodb.net/devTinder"
-  );
+const connectionDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://vyshnavivenkatesh:tOSVtWIW08RJgWYa@cluster0.j2l7cge.mongodb.net/devTinder"
+    )
+    console.log("db connection successful");
+  
+  } catch (err) {
+    console.log("error in db connection:", err.message);
+  }
 }
 
 module.exports = {
